46e5810efff7a02f3065ecdfea447341726f001f
Sebastian <mrseb10@gmail.com>
2024-05-10 10:49:04 +0200

    Fixed naming

--- NoSmoking.BBualdo.UI/src/app/app.routes.ts
+++ NoSmoking.BBualdo.UI/src/app/app.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from '../components/dashboard/dashboard.component';
+import { MainMenuComponent } from '../components/main-menu/main-menu.component';
 
-export const routes: Routes = [{ path: '', component: DashboardComponent }];
+export const routes: Routes = [{ path: '', component: MainMenuComponent }];
