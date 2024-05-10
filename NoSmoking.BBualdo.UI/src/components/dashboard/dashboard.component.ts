d69b88378247c2faca32cd6247e86d17ecf9fad7
Sebastian <mrseb10@gmail.com>
2024-05-10 11:57:23 +0200

    Smoke logs list view

--- NoSmoking.BBualdo.UI/src/components/dashboard/dashboard.component.ts
+++ NoSmoking.BBualdo.UI/src/components/dashboard/dashboard.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { SmokeService } from '../../services/smoke.service';
+import { SmokeLog } from '../../models/SmokeLog';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { AsyncPipe, formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
+  logsSubject: Subject<SmokeLog[] | null> = new BehaviorSubject<
+    SmokeLog[] | null
+  >(null);
 
+  logs$ = this.logsSubject.asObservable();
+
+  constructor(private smokeService: SmokeService) {}
+
+  ngOnInit(): void {
+    this.getSmokeLogs();
+  }
+
+  formatDate(dateStr: string, format: string, locale: string): string {
+    return formatDate(dateStr, format, locale);
+  }
+
+  getSmokeLogs() {
+    this.smokeService.getLogs().subscribe((logs) => {
+      this.logsSubject.next(logs);
+    });
+  }
 }
